a607a58f89abd45379e6bc08bbad59bdc695efc3
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-08-24 17:27:19 +0900

    User migration_file 作成 カラム追加。

--- db/migrate/20180824082013_devise_create_users.rb
+++ db/migrate/20180824082013_devise_create_users.rb
@@ -32,6 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :company_name
+      t.string :president_name
+      t.string :address
+
 
       t.timestamps null: false
     end
