4b025add11366e3da32f26c3befb654745875392
あ <a@MVP.local>
2018-09-16 02:31:11 +0900

    ログイン周り

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   	resources :tasks
   end
 
+  put '/tasks/:id/todo', to: 'tasks#todo'
+  put '/tasks/:id/doing', to: 'tasks#doing'
+  put '/tasks/:id/finish', to: 'tasks#finish'
+
+
   root to: 'users#index'
 end
