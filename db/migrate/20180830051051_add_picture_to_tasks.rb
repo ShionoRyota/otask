dfc54fa2cfcd14f4ffdff958ea728b1166f94419
あ <a@MVP.local>
2018-10-31 15:43:41 +0900

    不要な画像削除

--- db/migrate/20180830051051_add_picture_to_tasks.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToTasks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tasks, :picture, :string
-  end
-end
--- test/mailers/previews/term_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/term_mailer
-class TermMailerPreview < ActionMailer::Preview
-
-end
