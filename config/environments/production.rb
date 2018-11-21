4d31735f42f7c55b383cd079d56c8bdbfb1a1012
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-11-21 18:18:18 +0900

    production.rb の一行を削除。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:                 587,
