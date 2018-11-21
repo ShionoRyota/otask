94d09cdf88f198e3d21960c976cc0ca2048f0bdb
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-11-21 18:13:34 +0900

    production.rb に actionmailler の記述done.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,25 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'https://otask.herokuapp.com/' }
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            'otask.kouritsu2018@gmail.com',
+    password:             'nyvxaxuypfvqcoat',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
