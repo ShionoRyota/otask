cfdc5b360a0cc2460f7de670b4404601c5b710eb
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-11-28 15:20:32 +0900

    Env name 変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,8 +23,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY'],
     region: 'us-east-2'#リージョンをUS以外にしたかたはそのリージョンに変更
   }
 
