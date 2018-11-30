9c80f729bb083be12b6cae4e119b6c5e367cc9b2
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-11-30 12:25:08 +0900

    変更

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :number, :email, :id, :token, :name, :number, :postal_code, :address, :images]
+# Rails.application.config.filter_parameters += [:password, :number, :email, :id, :token, :name, :number, :postal_code, :address, :images]
+Rails.application.config.filter_parameters += [:password]
