4f3bf3cc1b64ecede92aaaf4e553f2b43f4acf63
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-11-30 16:15:16 +0900

    Image からむ追加

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_11_09_155137) do
     t.datetime "sale_time"
     t.integer "list_id"
     t.integer "user_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
