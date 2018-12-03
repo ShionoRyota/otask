3603dc2c68c6b8af4a826f730e4ae76b986334f7
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-12-03 13:23:58 +0900

    Recaptcha 機能 controllerに追記

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,6 +22,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     confirm_email_path
   end
+
+
+  def create
+    if verify_recaptcha
+      super
+    else
+      self.resource = resource_class.new
+      respond_with_navigational(resource) { render :new }
+    end
+  end
   # PUT /resource
   # def update
   #   super
