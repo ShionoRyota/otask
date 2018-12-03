a3a312ff320b16ffb2f26735159eb799b1693d54
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-12-03 13:28:22 +0900

    Model 編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,8 @@ class User < ApplicationRecord
   validate :password_complexity
 
  def password_complexity
-  return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
-  errors.add :password, "パスワードの強度が不足しています。パスワードの長さは8〜70文字とし、大文字と小文字と数字と記号をそれぞれ1文字以上含める必要があります。"
+  return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,70}$/
+  errors.add :password, "パスワードの強度が不足しています。パスワードの長さは8〜70文字とし、大文字と小文字と数字をそれぞれ1文字以上含める必要があります。"
  end
 
 end
