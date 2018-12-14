9a8aa8303b37161b4186c7f5e9dc75bbb174a54b
塩野涼太 <ryota@shionoryoutanoMacBook-Air.local>
2018-12-14 11:51:48 +0900

    Billed アクション追加して、請求済みの仕事を二択で分ける画面作成。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,121 +4,125 @@ class UsersController < ApplicationController
   before_action :no_card?, except: [:show, :pay, :delete_done] # show,pay,delete_done以外はクレカ登録してるか確認(課金者以外排除)
 
 # ホーム画面
-	def index
-	end
+def index
+end
 
 # user登録の記入内容確認画面
-	def show
-    @user = User.find(current_user[:id])
-	end
+def show
+  @user = User.find(current_user[:id])
+end
 
 # pay.jpと連携
-	def pay
-     Payjp.api_key = ENV['PAY_ID']
+def pay
+ Payjp.api_key = ENV['PAY_ID']
 
-     customer = Payjp::Customer.create(
-      description: 'OTask本番環境サービス利用者'
-     )
+ customer = Payjp::Customer.create(
+  description: 'OTask本番環境サービス利用者'
+  )
 
-     @user = User.find(current_user[:id])
-     @user.customer_id = customer['id']
-     @user.save
-
-
-
-     plan = Payjp::Plan.create(
-      amount: 2980,
-      currency: 'jpy',
-      interval: 'month',
-      name: 'otask',
-      trial_days: 30
-    )
+ @user = User.find(current_user[:id])
+ @user.customer_id = customer['id']
+ @user.save
 
 
-     Payjp::Subscription.create(
-      plan: plan,
-      customer: customer
-     )
 
-     charge = Payjp::Charge.create(
-       :amount => 2980,
-       :card => params['payjp-token'],
-       :currency => 'jpy'
-     )
+ plan = Payjp::Plan.create(
+  amount: 2980,
+  currency: 'jpy',
+  interval: 'month',
+  name: 'otask',
+  trial_days: 30
+  )
 
 
-    redirect_to users_path
+ Payjp::Subscription.create(
+  plan: plan,
+  customer: customer
+  )
 
-    flash[:notice] = "支払い完了"
+ charge = Payjp::Charge.create(
+   :amount => 2980,
+   :card => params['payjp-token'],
+   :currency => 'jpy'
+   )
 
-  end
 
-  def pay_delete
-    Payjp.api_key = ENV['PAY_ID']
-    @user = User.find(current_user[:id])
-    pay_id = @user.customer_id
-    customer = Payjp::Customer.retrieve(pay_id)
-    customer.delete
-    @user.destroy
-    redirect_to users_delete_done_path
-  end
+ redirect_to users_path
 
-  def delete_confirm
-  end
+ flash[:notice] = "支払い完了"
 
-  def delete_done
-  end
+end
+
+def pay_delete
+  Payjp.api_key = ENV['PAY_ID']
+  @user = User.find(current_user[:id])
+  pay_id = @user.customer_id
+  customer = Payjp::Customer.retrieve(pay_id)
+  customer.delete
+  @user.destroy
+  redirect_to users_delete_done_path
+end
+
+def delete_confirm
+end
+
+def delete_done
+end
 
 # 収支管理
-  def income_and_expenditure
-    @user = User.find(current_user[:id])
-    @income_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:sale)
-    @income_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:sale)
-    @income_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:sale)
+def income_and_expenditure
+  @user = User.find(current_user[:id])
+  @income_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:sale)
+  @income_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:sale)
+  @income_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:sale)
 
-    @expenditure_input_day = Expenditure.where(user_id: @user, updated_at: Time.zone.now.all_day).sum(:expenditure_money)
-    @material_cost_expenditure_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:material_cost)
-    @brokerage_fee_expenditure_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:brokerage_fee)
-    @processing_fee_expenditure_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:processing_fee)
-    @expenditure_day = @expenditure_input_day.to_i + @material_cost_expenditure_day.to_i + @brokerage_fee_expenditure_day.to_i + @processing_fee_expenditure_day.to_i
+  @expenditure_input_day = Expenditure.where(user_id: @user, updated_at: Time.zone.now.all_day).sum(:expenditure_money)
+  @material_cost_expenditure_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:material_cost)
+  @brokerage_fee_expenditure_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:brokerage_fee)
+  @processing_fee_expenditure_day = Task.where(user_id: @user, sale_time: Time.zone.now.all_day).sum(:processing_fee)
+  @expenditure_day = @expenditure_input_day.to_i + @material_cost_expenditure_day.to_i + @brokerage_fee_expenditure_day.to_i + @processing_fee_expenditure_day.to_i
 
-    @expenditure_input_month = Expenditure.where(user_id: @user, updated_at: Time.zone.now.all_month).sum(:expenditure_money)
-    @material_cost_expenditure_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:material_cost)
-    @brokerage_fee_expenditure_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:brokerage_fee)
-    @processing_fee_expenditure_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:processing_fee)
-    @expenditure_month = @expenditure_input_month.to_i + @material_cost_expenditure_month.to_i + @brokerage_fee_expenditure_month.to_i + @processing_fee_expenditure_month.to_i
+  @expenditure_input_month = Expenditure.where(user_id: @user, updated_at: Time.zone.now.all_month).sum(:expenditure_money)
+  @material_cost_expenditure_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:material_cost)
+  @brokerage_fee_expenditure_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:brokerage_fee)
+  @processing_fee_expenditure_month = Task.where(user_id: @user, sale_time: Time.zone.now.all_month).sum(:processing_fee)
+  @expenditure_month = @expenditure_input_month.to_i + @material_cost_expenditure_month.to_i + @brokerage_fee_expenditure_month.to_i + @processing_fee_expenditure_month.to_i
 
-    @expenditure_input_year = Expenditure.where(user_id: @user, updated_at: Time.zone.now.all_year).sum(:expenditure_money)
-    @material_cost_expenditure_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:material_cost)
-    @brokerage_fee_expenditure_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:brokerage_fee)
-    @processing_fee_expenditure_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:processing_fee)
-    @expenditure_year = @expenditure_input_year.to_i + @material_cost_expenditure_year.to_i + @brokerage_fee_expenditure_year.to_i + @processing_fee_expenditure_year.to_i
+  @expenditure_input_year = Expenditure.where(user_id: @user, updated_at: Time.zone.now.all_year).sum(:expenditure_money)
+  @material_cost_expenditure_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:material_cost)
+  @brokerage_fee_expenditure_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:brokerage_fee)
+  @processing_fee_expenditure_year = Task.where(user_id: @user, sale_time: Time.zone.now.all_year).sum(:processing_fee)
+  @expenditure_year = @expenditure_input_year.to_i + @material_cost_expenditure_year.to_i + @brokerage_fee_expenditure_year.to_i + @processing_fee_expenditure_year.to_i
 
-    @profits_day = @income_day - @expenditure_day
-    @profits_month = @income_month - @expenditure_month
-    @profits_year = @income_year - @expenditure_year
+  @profits_day = @income_day - @expenditure_day
+  @profits_month = @income_month - @expenditure_month
+  @profits_year = @income_year - @expenditure_year
 
-  end
+end
 
 
 # 支出管理
-  def expenditure
-  end
+def expenditure
+end
 
 # 売上履歴
-  def sale_history
-  end
+def sale_history
+end
 
 # 支出履歴
-  def expenditure_history
-  end
+def expenditure_history
+end
 
   #クレジット登録しているか確認
   def no_card?
-      @current_user = User.find(current_user[:id])
-      if @current_user.customer_id.nil?
-        redirect_to "/users/sign_in"
-      end
+    @current_user = User.find(current_user[:id])
+    if @current_user.customer_id.nil?
+      redirect_to "/users/sign_in"
+    end
+  end
+
+  #請求みの仕事で本日の履歴か全部の履歴かを選択する画面
+  def billed
   end
 
 # 売上履歴（できれば一つにまとめたい）
